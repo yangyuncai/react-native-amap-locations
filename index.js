24142dd6716de73e0fc54932b0f58a73caae9f6c
yang.yuncai <383235388@qq.com>
2018-05-06 21:09:11 +0800

    feature: export native API for javascript.

--- index.js
+++ index.js
@@ -1,6 +1,74 @@
+/**
+ * @description - react-native-amap-locations API
+ * @author - yang.yuncai <383234388@qq.com>
+ */
 
+// external
 import { NativeModules } from 'react-native';
 
-const { AMapReactNativeAmapLocations } = NativeModules;
+// scope
+const { AMapLocations } = NativeModules;
+const LocationMode = {
+  Battery_Saving: 'Battery_Saving',
+  Device_Sensors: 'Device_Sensors',
+  Hight_Accuracy: 'Hight_Accuracy'
+};
 
-export default AMapReactNativeAmapLocations;
+const LocationPurpose = {
+  SignIn: 'SignIn',
+  Sport: 'Sport',
+  Transport: 'Transport'
+};
+
+/**
+ * @typedef {object} Options
+ *
+ * @property {string} mode
+ * @property {string} protocol
+ * @property {string} purpose
+ */
+
+/**
+ * @description - normalize options
+ *
+ * @param {Options} options
+ *
+ * @return {Options}
+ */
+function validate(options) {
+  const nextOptions = {};
+  const { mode, purpose } = options;
+
+  nextOptions.mode = !!Reflect.get(LocationMode, mode) ?
+    mode :
+    LocationMode.Hight_Accuracy;
+
+  nextOptions.purpose = !!Reflect.get(LocationPurpose, purpose) ?
+    purpose :
+    LocationPurpose.SignIn;
+
+  return nextOptions;
+}
+
+/**
+ * @description - get position one time
+ * @param {Options} options
+ */
+function getCurrentPositionAsync(options = {}) {
+  const validOptions = validate(options);
+
+  return AMapLocations.getCurrentPositionAsync(validOptions);
+}
+
+/**
+ *
+ * @param {object} options
+ * @param {Function} callback
+ */
+function watchCurrentPositionAsync(options, callback) {
+}
+
+export default {
+  getCurrentPositionAsync,
+  watchCurrentPositionAsync
+};
